1079ffa2f7bb2db4409ff547385415679a545cc0
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-08-27 11:09:59 +0300

    better slope calculation; added visualization

--- pooling.py
+++ pooling.py
@@ -1,3 +1,5 @@
+import sys
+
 def pooling(profiles):
     return [sum(pos_vals) for pos_vals in zip(*profiles)]
 
@@ -11,15 +13,17 @@ def join_sorted(iter_1, iter_2, key=lambda x: x):
         k_2 = key(obj_2)
         while True:
             if k_1 == k_2:
-                yield (obj_1, obj_2)
+                yield (k_1, obj_1, obj_2)
                 obj_1 = next(iter_1)
                 obj_2 = next(iter_2)
                 k_1 = key(obj_1)
                 k_2 = key(obj_2)
             elif k_1 < k_2:
+                # print(f'join_sorted - skip {k_1} from first iterator', file=sys.stderr)
                 obj_1 = next(iter_1)
                 k_1 = key(obj_1)
             elif k_1 > k_2:
+                # print(f'join_sorted - skip {k_2} from second iterator', file=sys.stderr)
                 obj_2 = next(iter_2)
                 k_2 = key(obj_2)
     except StopIteration:
