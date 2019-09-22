0fa6c007bed88995bb61795e128a62c852e0043c
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-09-22 10:20:11 +0300

    extract profile now can yield profiles sorted by transcript_id (so we can run two parallel iterators over two files)

--- extract_profile.py
+++ extract_profile.py
@@ -8,20 +8,29 @@ def load_transcript_cds_info(cds_annotation_filename):
     transcript_infos = CodingTranscriptInfo.each_from_file(cds_annotation_filename)
     return {tr_info.transcript_id: tr_info  for tr_info in transcript_infos}
 
-def transcript_coverages_in_file(alignment_filename, cds_info_by_transcript):
+
+def transcript_coverages_in_file(alignment_filename, cds_info_by_transcript, sort_transcripts=False):
     bam = pybedtools.BedTool(alignment_filename)
     bg = bam.genome_coverage(bg=True)
-    for (transcript_id, iterator) in itertools.groupby(bg, lambda interval: interval.chrom):
+    if sort_transcripts:
+        bg = bg.sort()
+    for (transcript_id, bedgraph_intervals_iter) in itertools.groupby(bg, lambda interval: interval.chrom):
         if transcript_id not in cds_info_by_transcript:
+            # We don't know length of such transcripts
             continue
         transcript_info = cds_info_by_transcript[transcript_id]
-        profile = np.zeros(transcript_info.transcript_length, dtype=np.int)
-        for interval in iterator:
-            # Note! pybedtools returns coordinates of `interval` in [0-closed, 1-open) format
-            coverage = int(interval[3])
-            profile[interval.start : interval.stop] = coverage
+        profile = profile_by_bedgraph(bedgraph_intervals_iter, transcript_info.transcript_length)
         yield (transcript_info, profile)
 
+def profile_by_bedgraph(bedgraph_intervals_iter, transcript_length):
+    # Check: is it reasonable to use real-values or integer-valued coverages?
+    profile = np.zeros(transcript_length, dtype=np.int)
+    for interval in bedgraph_intervals_iter:
+        # Note! pybedtools use 0-based coordinates (when in integer representation).
+        # See https://daler.github.io/pybedtools/3-brief-examples.html and https://daler.github.io/pybedtools/intervals.html#zero-based-coords
+        coverage = int(interval[3])
+        profile[interval.start : interval.stop] = coverage
+    return profile
 
 cds_annotation_fn = sys.argv[1] # 'gencode.vM22.cds_features.tsv'
 alignment_filename = sys.argv[2] # 'METTL3_ribo_Coots2017_m_r2.bam'
@@ -30,4 +39,4 @@ header = '\t'.join([CodingTranscriptInfo.header(), 'cds_total_coverage'])
 print(header)
 for (transcript_info, profile) in transcript_coverages_in_file(alignment_filename, cds_info_by_transcript):
     cds_profile = transcript_info.cds_profile(profile)
-    print(transcript_info, cds_profile.sum(), sep='\t')
+    print(transcript_info, ','.join(map(str, cds_profile)), sep='\t')
