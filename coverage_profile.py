031c1d110560b68a51d59f0b864ddac613a3b9dd
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-09-26 12:58:44 +0300

    refactor so that transcript_coverages_in_file accepts alignment, not alignment filename

--- coverage_profile.py
+++ coverage_profile.py
@@ -1,17 +1,18 @@
 import pybedtools
 import itertools
+import sys
 import numpy as np
 import coreutils_sort
 
-def transcript_coverages_in_file(alignment_filename, cds_info_by_transcript, sort_transcripts=False):
-    bam = pybedtools.BedTool(alignment_filename)
+def transcript_coverages_in_file(alignment, cds_info_by_transcript, sort_transcripts=False):
     if sort_transcripts:
-        bg = bam.genome_coverage(bg=True).coreutils_sort(key=['1,1'], stream=True)
+        bedgraph = alignment.genome_coverage(bg=True).coreutils_sort(key=['1,1'], stream=True)
     else:
-        bg = bam.genome_coverage(bg=True, stream=True)
-    for (transcript_id, bedgraph_intervals_iter) in itertools.groupby(bg, lambda interval: interval.chrom):
+        bedgraph = alignment.genome_coverage(bg=True, stream=True)
+    for (transcript_id, bedgraph_intervals_iter) in itertools.groupby(bedgraph, lambda interval: interval.chrom):
         if transcript_id not in cds_info_by_transcript:
             # We don't know length of such transcripts
+            print(f'Warning: there are reads from unknown transcript `{transcript_id}` in `{alignment.fn}`', file=sys.stderr)
             continue
         transcript_info = cds_info_by_transcript[transcript_id]
         profile = profile_by_bedgraph(bedgraph_intervals_iter, transcript_info.transcript_length)
--- slope.py
+++ slope.py
@@ -6,23 +6,33 @@ from pooling import starjoin_sorted, pooling
 import pasio_wrapper
 import numpy as np
 import math
+from pybedtools import BedTool
 
-def slope_by_profiles(cds_profile_1, cds_profile_2, pooled_cds_coverage, splitter):
-    total_coverage_1 = sum(cds_profile_1)
-    total_coverage_2 = sum(cds_profile_2)
+def stabilize_profile(profile, segments):
+    stable_profile = np.zeros_like(profile)
+    for (start, stop, *_) in segments:
+        stable_profile[start:stop] = np.mean(profile[start:stop])
+    return stable_profile
+
+def slope_by_profiles(control_profile, experiment_profile, segments):
+    total_coverage_control = sum(control_profile)
+    total_coverage_experiment = sum(experiment_profile)
     model = sklearn.linear_model.LinearRegression()
     xs = []
     ys = []
     sample_weights = []
-    for (start, stop, _mean_count) in pasio_wrapper.stable_segments(pooled_cds_coverage, splitter):
-        mean_1 = np.mean(cds_profile_1[start:stop])
-        mean_2 = np.mean(cds_profile_2[start:stop])
-        coeff = ((mean_2 + 1) / (mean_1 + 1)) * ((total_coverage_1 + 1) / (total_coverage_2 + 1))
-        coeff = math.log(coeff)
+    num_segments = len(segments)
+    for (start, stop, *_) in segments:
+        mean_control = np.mean(control_profile[start:stop])
+        mean_experiment = np.mean(experiment_profile[start:stop])
+        normalized_mean_control = (mean_control + 1) / (total_coverage_control + num_segments)
+        normalized_mean_experiments = (mean_experiments + 1) / (total_coverage_experiments + num_segments)
+        detrended_profile = normalized_mean_experiments / normalized_mean_control
+        log_detrended_profile = math.log(detrended_profile)
         xs.append([(start + stop) / 2])
-        ys.append(coeff)
+        ys.append(log_detrended_profile)
         sample_weights.append(1)
-        # sample_weights.append(stop - start + 1)
+        # sample_weights.append(stop - start)
     model.fit(xs, ys, sample_weight=sample_weights)
     slope = model.coef_[0]
     return slope
@@ -34,18 +44,24 @@ cds_info_by_transcript = load_transcript_cds_info(cds_annotation_fn)
 
 alignment_1_fn = sys.argv[2]
 alignment_2_fn = sys.argv[3]
-coverages_1_iter = transcript_coverages_in_file(alignment_1_fn, cds_info_by_transcript, sort_transcripts=True)
-coverages_2_iter = transcript_coverages_in_file(alignment_2_fn, cds_info_by_transcript, sort_transcripts=True)
+
+alignment_1 = BedTool(alignment_1_fn)
+alignment_2 = BedTool(alignment_2_fn)
+
+coverages_1_iter = transcript_coverages_in_file(alignment_1, cds_info_by_transcript, sort_transcripts=True)
+coverages_2_iter = transcript_coverages_in_file(alignment_2, cds_info_by_transcript, sort_transcripts=True)
 
 for (transcript_info, (_, coverage_1), (_, coverage_2)) in starjoin_sorted(coverages_1_iter, coverages_2_iter, key=lambda txinfo, coverage: txinfo):
-    # print(transcript_info)
     cds_profile_1 = transcript_info.cds_profile(coverage_1)
     cds_profile_2 = transcript_info.cds_profile(coverage_2)
 
     pooled_cds_coverage = pooling([cds_profile_1, cds_profile_2])
+    segments = list( pasio_wrapper.stable_segments(pooled_cds_coverage, splitter) ) # [(start, stop, lambda), ...]
+    stable_cds_profile_1 = stabilize_profile(cds_profile_1, segments)
+    stable_cds_profile_2 = stabilize_profile(cds_profile_2, segments)
 
     try:
-        slope = slope_by_profiles(cds_profile_1, cds_profile_2, pooled_cds_coverage, splitter)
+        slope = slope_by_profiles(cds_profile_1, cds_profile_2, segments)
     except:
         # num_errors += 1
         print('Error', file = sys.stderr)
@@ -62,6 +78,10 @@ for (transcript_info, (_, coverage_1), (_, coverage_2)) in starjoin_sorted(cover
         transcript_info, slope,
     ]
     print(*field_values, sep='\t')
+    print(*stable_cds_profile_1, sep='\t')
+    print(*cds_profile_1, sep='\t')
+    print(*cds_profile_2, sep='\t')
+    print(*stable_cds_profile_2, sep='\t')
     # coverage_diffs.append( CoverageDifference(*field_values) )
     # print(f'Num errors: {num_errors}', file = sys.stderr)
     # return coverage_diffs
