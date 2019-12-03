60437054cfe90f6db7eb896b5ae7de036da0c06b
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-12-03 16:34:15 +0300

    refine extract_sequences not to load unnecessary attributes and to get stop codon into sequence

--- extract_sequences.py
+++ extract_sequences.py
@@ -4,9 +4,14 @@ from coding_transcript_info import CodingTranscriptInfo
 gtf_annotation_fn = 'source_data/annotation/gencode.vM22.basic.annotation.gtf.gz'
 assembly_fn = '/home/ilya/iogen/data/genome/mm10.fa'
 
-annotation = Annotation.load(gtf_annotation_fn, coding_only=True)
+annotation = Annotation.load(gtf_annotation_fn, coding_only=True, relevant_attributes=set())
 transcript_ids_list = annotation.transcript_by_id.keys()
 
-for transcript_id, seq in annotation.transcript_sequences(transcript_ids_list, assembly_fn, feature_type='cds'):
+# annotation = Annotation.load(gtf_annotation_fn, coding_only=True, relevant_attributes={'level', 'transcript_support_level'})
+# transcript_ids_list = annotation.transcript_by_id.keys()
+# transcript_ids_list = filter(lambda tr_id: annotation.transcript_by_id[tr_id].attributes['level'] in [1, 2], transcript_ids_list)
+# transcript_ids_list = filter(lambda tr_id: annotation.transcript_by_id[tr_id].attributes.get('transcript_support_level') in ['1', '2'], transcript_ids_list)
+
+for transcript_id, seq in annotation.transcript_sequences(transcript_ids_list, assembly_fn, feature_type='cds_with_stop'):
     print(f'>{transcript_id}')
     print(seq)
