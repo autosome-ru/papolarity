5a5ed0481356add02436df390f5cd31f8da3c023
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-12-04 16:42:25 +0300

    refactor profile extraction into alignment --> bedgraph; bedgraph --> numpy profiles

--- coverage_profile.py
+++ coverage_profile.py
@@ -7,11 +7,20 @@ import coreutils_sort
 def transcript_coverages_from_file(alignment_fn, sort_transcripts=False):
     alignment = BedTool(alignment_fn)
     yield from transcript_coverages_from_alignment(alignment, sort_transcripts=sort_transcripts)
+
 def transcript_coverages_from_alignment(alignment, sort_transcripts=False):
+    bedgraph = make_coverage(alignment, sort_transcripts=sort_transcripts)
+    yield from transcript_coverages_from_bedgraph(bedgraph)
+
+def make_coverage(alignment, sort_transcripts=False):
     if sort_transcripts:
+        # sort by chromosome (transcript_id in case of transcriptomic alignments)
         bedgraph = alignment.genome_coverage(bga=True).coreutils_sort(key=['1,1'], stream=True)
     else:
         bedgraph = alignment.genome_coverage(bga=True, stream=True)
+    return bedgraph
+
+def transcript_coverages_from_bedgraph(bedgraph):
     for (transcript_id, bedgraph_intervals_iter) in itertools.groupby(bedgraph, lambda interval: interval.chrom):
         bedgraph_intervals = list(bedgraph_intervals_iter)
         transcript_length = max(interval.stop for interval in bedgraph_intervals)
