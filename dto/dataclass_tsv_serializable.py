1f8501c3b9015a8fbf50a1302d477e02c2d966d8
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-12-08 18:29:52 +0300

    now can work with classes w/o header;  now works as an alias of

--- dto/dataclass_tsv_serializable.py
+++ dto/dataclass_tsv_serializable.py
@@ -1,3 +1,4 @@
+import sys
 import dataclasses
 
 @dataclasses.dataclass
@@ -8,6 +9,9 @@ class DataclassTsvSerializable:
         return '\t'.join(fields)
 
     def __str__(self):
+        return self.tsv_string()
+
+    def tsv_string(self):
         fields = [getattr(self, field.name) for field in dataclasses.fields(self)]
         return '\t'.join(map(str, fields))
 
@@ -17,8 +21,16 @@ class DataclassTsvSerializable:
         return cls(*[field.type(value) for field, value in zip(dataclasses.fields(cls), row)])
 
     @classmethod
-    def each_in_file(cls, filename):
+    def each_in_file(cls, filename, header=True):
         with open(filename) as f:
-            header = f.readline()
+            if header:
+                f.readline() # skip header
             for line in f:
                 yield cls.from_string(line)
+
+    @classmethod
+    def print_tsv(cls, collection, file=sys.stdout, header=True):
+        if header:
+            print(cls.header(), file=file)
+        for record in collection:
+            print(record.tsv_string(), file=file)
