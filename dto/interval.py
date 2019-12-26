6059b0705a270ec0c4cb026916d220294dfa0963
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-12-26 20:19:25 +0300

    Added ordering to intervals, force segmentation ordering. Added validators for Interval/Segmentation instantiation

--- dto/interval.py
+++ dto/interval.py
@@ -1,8 +1,19 @@
 import dataclasses
 from dto.dataclass_tsv_serializable import DataclassTsvSerializable
 
-@dataclasses.dataclass
+@dataclasses.dataclass(order=True)
 class Interval(DataclassTsvSerializable):
+    '''bed-coordinates [a, b)'''
     chrom: str
-    start:int
+    start: int
     stop: int
+
+    def __post_init__(self):
+        if not isinstance(self.chrom, str):
+            raise TypeError(f'Chromosome should be a string but was `{self.chrom}`')
+        if not isinstance(self.start, int):
+            raise TypeError(f'Interval start should be integer but was `{self.start}`')
+        if not isinstance(self.stop, int):
+            raise TypeError(f'Interval stop should be integer but was `{self.stop}`')
+        if self.start >= self.stop:
+            raise ValueError(f'Interval start={self.start} should be less than stop={self.stop}')
