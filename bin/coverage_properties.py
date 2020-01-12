f39de36a7791d6b9b2823743c516af0dd72c2909
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-12 12:46:34 +0300

    make use of open_for_write in coverage_properties

--- bin/coverage_properties.py
+++ bin/coverage_properties.py
@@ -3,6 +3,7 @@ from os.path import dirname
 sys.path.insert(0, dirname(dirname(__file__)))
 import argparse
 import numpy as np
+from gzip_utils import open_for_write
 from dto.transcript_coverage import TranscriptCoverage
 from polarity_score import polarity_score
 
@@ -12,6 +13,7 @@ def get_argparser():
         description = "Calculate coverage profile properties",
     )
     argparser.add_argument('coverage', metavar='coverage.bedgraph', help='Coverage data')
+    argparser.add_argument('--output-file', '-o', dest='output_file', help="Store results at this path")
     return argparser
 
 argparser = get_argparser()
@@ -19,14 +21,17 @@ args = argparser.parse_args()
 
 coverages = TranscriptCoverage.each_in_file(args.coverage, header=False, dtype=int)
 
-for transcript_coverage in coverages:
-    coverage = transcript_coverage.coverage
-    
-    transcript_id = transcript_coverage.transcript_id
-    transcript_length = len(coverage)
-    mean_coverage = np.mean(coverage)
-    total_coverage = np.sum(coverage)
-    polarity = polarity_score(coverage)
-    
-    info = [transcript_id, transcript_length, mean_coverage, total_coverage, polarity]
-    print('\t'.join(map(str, info)))
+with open_for_write(args.output_file) as output_stream:
+    header = ['transcript_id', 'transcript_length', 'mean_coverage', 'total_coverage', 'polarity']
+    print('\t'.join(header), file=output_stream)
+    for transcript_coverage in coverages:
+        coverage = transcript_coverage.coverage
+
+        transcript_id = transcript_coverage.transcript_id
+        transcript_length = len(coverage)
+        mean_coverage = np.mean(coverage)
+        total_coverage = np.sum(coverage)
+        polarity = polarity_score(coverage)
+
+        info = [transcript_id, transcript_length, mean_coverage, total_coverage, polarity]
+        print('\t'.join(map(str, info)), file=output_stream)
