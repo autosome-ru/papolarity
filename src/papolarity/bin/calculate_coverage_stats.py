6b36afd05aed9c502b3dba5f7994a134c83381eb
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-14 23:36:53 +0300

    added --output-file option in calculate_coverage_stats

--- src/papolarity/bin/calculate_coverage_stats.py
+++ src/papolarity/bin/calculate_coverage_stats.py
@@ -1,5 +1,6 @@
 import argparse
 from ..utils import common_subsequence
+from ..gzip_utils import open_for_write
 from ..dto.transcript_coverage import TranscriptCoverage
 from ..segmentation import Segmentation
 from ..dto.coverage_comparison_stats import CoverageComparisonStats
@@ -30,6 +31,7 @@ def configure_argparser(argparser=None):
     argparser.add_argument('segmentation', metavar='segmentation.bed', help='Segmentation')
     argparser.add_argument('coverage_control', metavar='control.bedgraph', help='Coverage for control data')
     argparser.add_argument('coverage_experiment', metavar='experiment.bedgraph', help='Coverage for experiment data')
+    argparser.add_argument('--output-file', '-o', dest='output_file', help="Store results at this path")
     return argparser
 
 def main():
@@ -44,4 +46,5 @@ def invoke(args):
     experiment_coverages = TranscriptCoverage.each_in_file(args.coverage_experiment, header=False, dtype=int)
 
     transcript_comparison_infos = compare_coverage_streams(segmentation_stream, control_coverages, experiment_coverages)
-    CoverageComparisonStats.print_tsv(transcript_comparison_infos, header=True)
+    with open_for_write(args.output_file) as output_stream:
+        CoverageComparisonStats.print_tsv(transcript_comparison_infos, header=True, file=output_stream)
