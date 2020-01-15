f734ea77c4e7d72d9e05cc6622a5d180c29b196b
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-15 22:48:41 +0300

    added optional prefix for coverage_properties

--- src/papolarity/bin/coverage_properties.py
+++ src/papolarity/bin/coverage_properties.py
@@ -8,6 +8,7 @@ def configure_argparser(argparser=None):
     if not argparser:
         argparser = argparse.ArgumentParser(prog="coverage_properties", description="Calculate coverage profile properties")
     argparser.add_argument('coverage', metavar='coverage.bedgraph', help='Coverage data')
+    argparser.add_argument('--prefix', default='', help='Prefix of property columns (to distinguish control and experiment)')
     argparser.add_argument('--output-file', '-o', dest='output_file', help="Store results at this path")
     return argparser
 
@@ -19,7 +20,9 @@ def main():
 def invoke(args):
     coverages = TranscriptCoverage.each_in_file(args.coverage, header=False, dtype=int)
     with open_for_write(args.output_file) as output_stream:
-        header = ['transcript_id', 'transcript_length', 'mean_coverage', 'total_coverage', 'polarity']
+        property_names = ['transcript_length', 'mean_coverage', 'total_coverage', 'polarity']
+        prefixed_property_names = [f'{args.prefix}{name}' for name in property_names]
+        header = ['transcript_id', *prefixed_property_names]
         print('\t'.join(header), file=output_stream)
         for transcript_coverage in coverages:
             coverage = transcript_coverage.coverage
