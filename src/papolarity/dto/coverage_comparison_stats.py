0d8e13633178567a9b854e01c708a6b205f8be18
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-15 17:11:06 +0300

    refactor and optimize slope calculation

--- src/papolarity/dto/coverage_comparison_stats.py
+++ src/papolarity/dto/coverage_comparison_stats.py
@@ -6,15 +6,18 @@ from ..profile_comparison import profile_difference, slope_by_profiles
 class CoverageComparisonStats(DataclassTsvSerializable):
     transcript_id: str
     slope: float
+    weighted_slope: float
     multipoint_slope: float
     profile_difference: float
 
     @classmethod
     def make_from_profiles(cls, transcript_id, profile_control, profile_experiment, segmentation):
+        slopes = slope_by_profiles(profile_control, profile_experiment, segmentation)
         info = {
             'transcript_id': transcript_id,
-            'slope': slope_by_profiles(profile_control, profile_experiment, segmentation, mode='center'),
-            'multipoint_slope': slope_by_profiles(profile_control, profile_experiment, segmentation, mode='every_point'),
+            'slope': slopes['center'],
+            'weighted_slope': slopes['weighted_center'],
+            'multipoint_slope': slopes['every_point'],
             'profile_difference': profile_difference(profile_control, profile_experiment, segmentation),
         }
         return cls(**info)
--- src/papolarity/profile_comparison.py
+++ src/papolarity/profile_comparison.py
@@ -2,64 +2,81 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 from math import log
 
-def slope_by_profiles(control_profile, experiment_profile, segmentation, mode='center'):
-    total_coverage_control = sum(control_profile)
-    total_coverage_experiment = sum(experiment_profile)
+from collections import namedtuple
+WeigthedPoints = namedtuple('WeigthedPoints', ['xs', 'ys', 'weights'])
+
+def slope_by_profiles(control_profile, experiment_profile, segmentation):
+    cumsum_coverage_control = np.hstack([0, np.cumsum(control_profile)])
+    cumsum_coverage_experiment = np.hstack([0, np.cumsum(experiment_profile)])
+    total_coverage_control = cumsum_coverage_control[-1]
+    total_coverage_experiment = cumsum_coverage_experiment[-1]
     assert len(control_profile) == len(experiment_profile)
     assert len(control_profile) == segmentation.segmentation_length
     profile_len = len(control_profile)
-    model = LinearRegression()
-    xs = []
-    ys = []
-    sample_weights = []
+
+    center_info = WeigthedPoints([], [], [])
+    weighted_center_info = WeigthedPoints([], [], [])
+    every_point_info = WeigthedPoints([], [], [])
+
     num_segments = segmentation.num_segments
     for segment in segmentation.segments:
         start = segment.start
         stop = segment.stop
-        mean_control = np.mean(control_profile[start:stop])
-        mean_experiment = np.mean(experiment_profile[start:stop])
+        mean_control = (cumsum_coverage_control[stop] - cumsum_coverage_control[start]) / (stop - start)
+        mean_experiment = (cumsum_coverage_experiment[stop] - cumsum_coverage_experiment[start]) / (stop - start)
         normalized_mean_control = (mean_control + 1) / (total_coverage_control + num_segments)
         normalized_mean_experiment = (mean_experiment + 1) / (total_coverage_experiment + num_segments)
         detrended_profile = normalized_mean_experiment / normalized_mean_control
-        log_detrended_profile = log(detrended_profile)
-        if mode == 'center':
-            coord = (start + stop - 1) / 2
-            rel_coord = coord / profile_len
-            xs.append(rel_coord)
-            ys.append(log_detrended_profile)
-            sample_weights.append(1)
-        elif mode == 'weighted_center':
-            coord = (start + stop - 1) / 2
-            rel_coord = coord / profile_len
-            weight = stop - start
-            xs.append(rel_coord)
-            ys.append(log_detrended_profile)
-            sample_weights.append(weight)
-        elif mode == 'every_point':
-            for pos in range(start, stop):
-                rel_coord = pos / profile_len
-                xs.append(rel_coord)
-                ys.append(log_detrended_profile)
-                sample_weights.append(1)
-        else:
-            raise NotImplementedError()
-        # sample_weights.append(stop - start)
+        value = log(detrended_profile)
+
+        coord = (start + stop - 1) / 2
+        rel_coord = coord / profile_len
+
+        # mode: 'center'
+        center_info.xs.append(rel_coord)
+        center_info.ys.append(value)
+        center_info.weights.append(1)
+
+        # mode: 'weighted_center'
+        weight = stop - start
+        weighted_center_info.xs.append(rel_coord)
+        weighted_center_info.ys.append(value)
+        weighted_center_info.weights.append(weight)
+
+        # mode: 'every_point'
+        for pos in range(start, stop):
+            rel_pos = pos / profile_len
+            every_point_info.xs.append(rel_pos)
+            every_point_info.ys.append(value)
+            every_point_info.weights.append(1)
+
+    return {
+        'center': slope_by_points(center_info.xs, center_info.ys, center_info.weights),
+        'weighted_center': slope_by_points(weighted_center_info.xs, weighted_center_info.ys, weighted_center_info.weights),
+        'every_point': slope_by_points(every_point_info.xs, every_point_info.ys, every_point_info.weights),
+    }
+
+def slope_by_points(xs, ys, weights):
     xs = np.array(xs)
-    model.fit(xs.reshape(-1, 1), ys, sample_weight=sample_weights)
-    slope = model.coef_[0]
-    return slope
+    model = LinearRegression()
+    model.fit(xs.reshape(-1, 1), ys, sample_weight=weights)
+    return model.coef_[0]
 
 def profile_difference(control_profile, experiment_profile, segmentation):
     assert len(control_profile) == len(experiment_profile)
-    control_profile_sum = np.sum(control_profile)
-    experiment_profile_sum = np.sum(experiment_profile)
-    normed_control_profile = control_profile / control_profile_sum  if control_profile_sum != 0  else control_profile
-    normed_experiment_profile = experiment_profile / experiment_profile_sum  if experiment_profile_sum != 0  else experiment_profile
+
+    cumsum_coverage_control = np.hstack([0, np.cumsum(control_profile)])
+    cumsum_coverage_experiment = np.hstack([0, np.cumsum(experiment_profile)])
+    control_profile_sum = cumsum_coverage_control[-1]
+    experiment_profile_sum = cumsum_coverage_experiment[-1]
+
+    cumsum_coverage_control_normed = cumsum_coverage_control / control_profile_sum  if control_profile_sum != 0  else cumsum_coverage_control
+    cumsum_coverage_experiment_normed = cumsum_coverage_experiment / experiment_profile_sum  if experiment_profile_sum != 0  else cumsum_coverage_experiment
     difference = 0
     for segment in segmentation.segments:
         start = segment.start
         stop = segment.stop
-        mean_control = np.mean(normed_control_profile[start:stop])
-        mean_experiment = np.mean(normed_experiment_profile[start:stop])
-        difference += (stop - start) * abs(mean_control - mean_experiment)
+        control_segment = cumsum_coverage_control_normed[stop] - cumsum_coverage_control_normed[start]
+        experiment_segment = cumsum_coverage_experiment_normed[stop] - cumsum_coverage_experiment_normed[start]
+        difference += abs(control_segment - experiment_segment)
     return difference / len(control_profile)
