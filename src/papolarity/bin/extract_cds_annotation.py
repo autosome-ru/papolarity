14c173ca498f36d7f6fd9e883a87f48fbfcc3613
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-17 15:30:28 +0300

    complex and overengineered filter which can work with different feature types separately

--- src/papolarity/bin/extract_cds_annotation.py
+++ src/papolarity/bin/extract_cds_annotation.py
@@ -3,21 +3,33 @@ from ..gzip_utils import open_for_write
 from ..annotation import Annotation
 from ..dto.coding_transcript_info import CodingTranscriptInfo
 
+# gene:attr=value,value,value
+# transcript:attr=value,value,value
+# *:attr=value,value,value
 def parse_condition(condition_str):
-    k,vs = condition_str.split('=')
-    return (k, vs.split(','))
+    feature_types, attr_condition_str = condition_str.split(':', maxsplit=1)
+    k, vs = attr_condition_str.split('=', maxsplit=1)
+    return {'attr': k, 'acceptable_values': vs.split(','), 'feature_types': feature_types.split(',')}
 
-# Known issue: filters now treat all attribute values as strings
-def create_record_filter(condition_config):
-    k,vs = condition_config
-    return lambda rec: str(rec.attributes.get(k)) in vs
+# Known issue: filters treat all attribute values as strings
+def create_attr_filter(condition_config):
+    feature_types = condition_config['feature_types']
+    k = condition_config['attr']
+    vs = condition_config['acceptable_values']
+    if '*' in feature_types:
+        return lambda rec: str(rec.attributes.get(k, '')) in vs
+    else:
+        return lambda rec: ((str(rec.attributes.get(k, '')) in vs) and (rec.type in feature_types)) or (rec.type not in feature_types)
 
 def configure_argparser(argparser=None):
     if not argparser:
         argparser = argparse.ArgumentParser(prog="extract_cds_annotation", description = "Extract CDS annotation in transcriptomic coordinates from genomic annotation")
     argparser.add_argument('gtf_annotation', metavar='annotation.gtf', help='Genomic annotation in GTF-format')
     argparser.add_argument('--output-file', '-o', dest='output_file', help="Store results at this path")
-    argparser.add_argument('--filter', action='append', dest='filters', default=[], help="Store results at this path")
+    argparser.add_argument('--attr-filter', action='append', dest='attr_filters', default=[],
+                                            help="Filter records so that attributes has one of specified values.\n"
+                                                 "Filter is applied only to records of specified feature type (`*` for all feature types).\n"
+                                                 "Format: `type1,type2:tag=value1,value2,...`")
     # Known issue: Right now all multivalue keys are ignored, so filters won't work with them
     # ToDo: add ability to specify multivalue keys and add filters like `--filter-has tag value`
     return argparser
@@ -28,10 +40,10 @@ def main():
     invoke(args)
 
 def invoke(args):
-    condition_configs = [parse_condition(condition_str) for condition_str in args.filters]
-    filters = [create_record_filter(condition_config) for condition_config in condition_configs]
+    condition_configs = [parse_condition(condition_str) for condition_str in args.attr_filters]
+    filters = [create_attr_filter(condition_config) for condition_config in condition_configs]
     filter_conjuction = lambda rec: all(f(rec) for f in filters)
-    relevant_attributes = {k for (k,v) in condition_configs}
+    relevant_attributes = {config['attr'] for config in condition_configs}
 
     annotation = Annotation.load(
         args.gtf_annotation,
