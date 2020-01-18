aa0e2fe2f4ba5b3a8f690c3d9a991f0d26ec6e97
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-18 15:21:19 +0300

    rename sample_bns --> samples etc in protocol.sh

--- protocol.sh
+++ protocol.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SAMPLE_BNS='ES_noHR_noCH_ribo  ES_noHR_60sCH_ribo  ES_90sHR_60sCH_ribo  ES_120sHR_60sCH_ribo  ES_150sHR_60sCH_ribo  ES_180sHR_60sCH_ribo';
+SAMPLES='ES_noHR_noCH_ribo  ES_noHR_60sCH_ribo  ES_90sHR_60sCH_ribo  ES_120sHR_60sCH_ribo  ES_150sHR_60sCH_ribo  ES_180sHR_60sCH_ribo';
 
 # 3.1. Common preprocessing
 
@@ -27,10 +27,10 @@ csvtk --tabs cut genome/gencode.vM23.cds_features.tsv \
 
 mkdir -p ./coverage/;
 (
-  for SAMPLE_BN in $SAMPLE_BNS; do
-    echo papolarity get_coverage "./align/${SAMPLE_BN}.bam" \
+  for SAMPLE in $SAMPLES; do
+    echo papolarity get_coverage "./align/${SAMPLE}.bam" \
                     --sort --dtype int \
-                    --output-file "./coverage/${SAMPLE_BN}.bedgraph.gz" ;
+                    --output-file "./coverage/${SAMPLE}.bedgraph.gz" ;
   done
 ) | parallel
 
@@ -44,13 +44,13 @@ papolarity pool_coverage ./coverage/*.bedgraph.gz --dtype int --output-file ./co
 
 mkdir -p ./cds_coverage;
 (
-  for SAMPLE_BN in $SAMPLE_BNS 'pooled'; do
+  for SAMPLE in $SAMPLES 'pooled'; do
     echo papolarity clip_cds \
                     ./genome/gencode.vM23.cds_features.tsv \
-                    "./coverage/${SAMPLE_BN}.bedgraph.gz" \
+                    "./coverage/${SAMPLE}.bedgraph.gz" \
                     --drop-5-flank 15  --drop-3-flank 15 \
                     --contig-naming original \
-                    --output-file "./cds_coverage/${SAMPLE_BN}.bedgraph.gz" ;
+                    --output-file "./cds_coverage/${SAMPLE}.bedgraph.gz" ;
   done
 ) | parallel
 
@@ -63,11 +63,11 @@ mkdir -p ./cds_coverage;
 
 mkdir -p ./coverage_features/raw;
 (
-  for SAMPLE_BN in $SAMPLE_BNS 'pooled'; do
+  for SAMPLE in $SAMPLES 'pooled'; do
     echo papolarity coverage_features \
-                    "./cds_coverage/${SAMPLE_BN}.bedgraph.gz" \
-                    --prefix "${SAMPLE_BN}_" \
-                    --output-file "./coverage_features/raw/${SAMPLE_BN}.tsv"
+                    "./cds_coverage/${SAMPLE}.bedgraph.gz" \
+                    --prefix "${SAMPLE}_" \
+                    --output-file "./coverage_features/raw/${SAMPLE}.tsv"
   done
 ) | parallel
 
@@ -103,39 +103,39 @@ csvtk --tabs cut \
 # 3.2.3. Finalizing the polarity score lists
 
 mkdir -p ./coverage_features/filtered;
-for SAMPLE_BN in $SAMPLE_BNS; do
+for SAMPLE in $SAMPLES; do
     csvtk --tabs join \
         ./transcripts_list.tsv \
-        "./coverage_features/raw/${SAMPLE_BN}.tsv" \
-        --out-file "./coverage_features/filtered/${SAMPLE_BN}.tsv"
+        "./coverage_features/raw/${SAMPLE}.tsv" \
+        --out-file "./coverage_features/filtered/${SAMPLE}.tsv"
 done
 
 # 3.2.4. Polarity Z-score estimation
 
 mkdir -p ./coverage_features/adjusted;
-for SAMPLE_BN in $SAMPLE_BNS; do
+for SAMPLE in $SAMPLES; do
     papolarity adjust_features \
         "./coverage_features/filtered/${SAMPLE}.tsv" \
         --sort-field 'cds_length' \
-        --fields "${SAMPLE_BN}_polarity" \
+        --fields "${SAMPLE}_polarity" \
         --mode z-score \
         --window 500 \
         --prefix 'zscore_' \
-        --output-file "./coverage_features/adjusted/${SAMPLE_BN}.tsv"
+        --output-file "./coverage_features/adjusted/${SAMPLE}.tsv"
 done
 
 # 3.2.5. Plot per-sample polarity score distribution
 
 mkdir -p ./coverage_features/plot/;
-for SAMPLE_BN in $SAMPLE_BNS; do
+for SAMPLE in $SAMPLES; do
     papolarity plot_distribution \
-        "coverage_features/filtered/${SAMPLE_BN}.tsv" \
-        --fields "${SAMPLE_BN}_polarity" \
+        "coverage_features/filtered/${SAMPLE}.tsv" \
+        --fields "${SAMPLE}_polarity" \
         --no-legend \
-        --title "${SAMPLE_BN} polarity distribution" \
+        --title "${SAMPLE} polarity distribution" \
         --zero-line green \
         --xlim -1.0 1.0 \
-        --output-file "coverage_features/plot/${SAMPLE_BN}.png"
+        --output-file "coverage_features/plot/${SAMPLE}.png"
 done
 
 # 3.2.6. (supplementary step) Plot polarity score distribution for all samples on a single figure
@@ -143,14 +143,14 @@ done
 # Note: coverage_features/adjusted/all.tsv is not perfectly formatted - it has several identical columns. 
 # We use it for the only reason - to draw the plot.
 
-SAMPLE_FILES=$( echo $SAMPLE_BNS | xargs -n1 echo | xargs -n1 -I{} echo 'coverage_features/adjusted/{}.tsv' | tr '\n' ' ' )
+SAMPLE_FILES=$( echo $SAMPLES | xargs -n1 echo | xargs -n1 -I{} echo 'coverage_features/adjusted/{}.tsv' | tr '\n' ' ' )
 
 csvtk --tabs join \
     ./transcripts_list.tsv \
     $SAMPLE_FILES \
     --out-file coverage_features/adjusted/all.tsv;
 
-SAMPLE_FIELDS=$( echo $SAMPLE_BNS | xargs -n1 echo | xargs -n1 -I{} echo '{}_polarity' | tr '\n' ' ' );
+SAMPLE_FIELDS=$( echo $SAMPLES | xargs -n1 echo | xargs -n1 -I{} echo '{}_polarity' | tr '\n' ' ' );
 
 papolarity plot_distribution \
     "coverage_features/adjusted/all.tsv" \
@@ -180,90 +180,90 @@ papolarity clip_cds \
 
 mkdir -p ./coverage_flattened;
 (
-  for SAMPLE_BN in $SAMPLE_BNS; do
+  for SAMPLE in $SAMPLES; do
     echo papolarity flatten_coverage \
                     ./segmentation.bed.gz \
-                    "./coverage/${SAMPLE_BN}.bedgraph.gz" \
+                    "./coverage/${SAMPLE}.bedgraph.gz" \
                     --only-matching \
-                    --output-file "./coverage_flattened/${SAMPLE_BN}.bedgraph.gz";
+                    --output-file "./coverage_flattened/${SAMPLE}.bedgraph.gz";
   done
 ) | parallel
 
 # 3.3.4. Calculate slope for a pair of samples.
 
-CONTROL_BN='ES_noHR_noCH_ribo';
-EXPERIMENT_BNS='ES_noHR_60sCH_ribo  ES_90sHR_60sCH_ribo  ES_120sHR_60sCH_ribo  ES_150sHR_60sCH_ribo  ES_180sHR_60sCH_ribo';
+CONTROL='ES_noHR_noCH_ribo';
+EXPERIMENTS='ES_noHR_60sCH_ribo  ES_90sHR_60sCH_ribo  ES_120sHR_60sCH_ribo  ES_150sHR_60sCH_ribo  ES_180sHR_60sCH_ribo';
 
 mkdir -p ./comparison/raw;
 (
-  for EXPERIMENT_BN in $EXPERIMENT_BNS; do
+  for EXPERIMENT in $EXPERIMENTS; do
       echo papolarity compare_coverage \
                       ./cds_segmentation.bed.gz \
-                      "./cds_coverage/${CONTROL_BN}.bedgraph.gz" \
-                      "./cds_coverage/${EXPERIMENT_BN}.bedgraph.gz" \
-                      --prefix "${EXPERIMENT_BN}_" \
-                      --output-file "comparison/raw/${EXPERIMENT_BN}.tsv"
+                      "./cds_coverage/${CONTROL}.bedgraph.gz" \
+                      "./cds_coverage/${EXPERIMENT}.bedgraph.gz" \
+                      --prefix "${EXPERIMENT}_" \
+                      --output-file "comparison/raw/${EXPERIMENT}.tsv"
   done
 ) | parallel
 
 # 3.3.5. Finalizing profile comparison statistics
 
 mkdir -p ./comparison/filtered;
-for EXPERIMENT_BN in $EXPERIMENT_BNS; do
+for EXPERIMENT in $EXPERIMENTS; do
     csvtk --tabs join \
         ./transcripts_list.tsv \
-        "comparison/raw/${EXPERIMENT_BN}.tsv" \
-        --out-file "./comparison/filtered/${EXPERIMENT_BN}.tsv"
+        "comparison/raw/${EXPERIMENT}.tsv" \
+        --out-file "./comparison/filtered/${EXPERIMENT}.tsv"
 done
 
 # 3.3.6. Adjust comparison statistics
 
 mkdir -p ./comparison/adjusted;
-for EXPERIMENT_BN in $EXPERIMENT_BNS; do
+for EXPERIMENT in $EXPERIMENTS; do
     papolarity adjust_features \
         "comparison/filtered/${EXPERIMENT}.tsv" \
         --sort-field 'cds_length' \
-        --fields "${EXPERIMENT_BN}_slope" "${EXPERIMENT_BN}_logslope" "${EXPERIMENT_BN}_discrepancy" \
+        --fields "${EXPERIMENT}_slope" "${EXPERIMENT}_logslope" "${EXPERIMENT}_discrepancy" \
         --mode z-score \
         --window 500 \
         --prefix 'zscore_' \
-        --output-file "./comparison/adjusted/${EXPERIMENT_BN}.tsv"
+        --output-file "./comparison/adjusted/${EXPERIMENT}.tsv"
 done
 
 # 3.3.7. Plot per-sample distributions of slope
 
 mkdir -p ./comparison/plot;
-for EXPERIMENT_BN in $EXPERIMENT_BNS; do
+for EXPERIMENT in $EXPERIMENTS; do
     papolarity plot_distribution \
-        "comparison/adjusted/${EXPERIMENT_BN}.tsv" \
-        --fields "${EXPERIMENT_BN}_slope" \
+        "comparison/adjusted/${EXPERIMENT}.tsv" \
+        --fields "${EXPERIMENT}_slope" \
         --no-legend \
         --title 'Slope distribution' \
         --zero-line green \
         --xlim -100.0 100.0 \
-        --output-file "./comparison/plot/${EXPERIMENT_BN}_slope.png"
+        --output-file "./comparison/plot/${EXPERIMENT}_slope.png"
 done
 
-for EXPERIMENT_BN in $EXPERIMENT_BNS; do
+for EXPERIMENT in $EXPERIMENTS; do
     papolarity plot_distribution \
-        "comparison/adjusted/${EXPERIMENT_BN}.tsv" \
-        --fields "${EXPERIMENT_BN}_logslope" \
+        "comparison/adjusted/${EXPERIMENT}.tsv" \
+        --fields "${EXPERIMENT}_logslope" \
         --no-legend \
         --title 'Logarithmic slope distribution' \
         --zero-line green \
         --xlim -10 10 \
-        --output-file "./comparison/plot/${EXPERIMENT_BN}_logslope.png"
+        --output-file "./comparison/plot/${EXPERIMENT}_logslope.png"
 done
 
-for EXPERIMENT_BN in $EXPERIMENT_BNS; do
+for EXPERIMENT in $EXPERIMENTS; do
     papolarity plot_distribution \
-        "comparison/adjusted/${EXPERIMENT_BN}.tsv" \
-        --fields "${EXPERIMENT_BN}_discrepancy" \
+        "comparison/adjusted/${EXPERIMENT}.tsv" \
+        --fields "${EXPERIMENT}_discrepancy" \
         --no-legend \
         --title 'Distribution of discrepancies' \
         --zero-line green \
         --xlim 0 2 \
-        --output-file "./comparison/plot/${EXPERIMENT_BN}_discrepancy.png"
+        --output-file "./comparison/plot/${EXPERIMENT}_discrepancy.png"
 done
 
 # 3.3.8. (supplementary step) Plot distributions of slopes distribution for all samples on a single figure
@@ -271,7 +271,7 @@ done
 # Note: comparison/adjusted/all.tsv is not perfectly formatted - it has several identical columns. 
 # We use it for the only reason - to draw the plot.
 
-SAMPLE_FILES=$( echo $EXPERIMENT_BNS | xargs -n1 echo | xargs -n1 -I{} echo 'comparison/adjusted/{}.tsv' | tr '\n' ' ' )
+SAMPLE_FILES=$( echo $EXPERIMENTS | xargs -n1 echo | xargs -n1 -I{} echo 'comparison/adjusted/{}.tsv' | tr '\n' ' ' )
 
 csvtk --tabs join \
     ./transcripts_list.tsv \
@@ -279,7 +279,7 @@ csvtk --tabs join \
     --out-file comparison/adjusted/all.tsv;
 
 for FIELD in slope logslope discrepancy; do
-    SAMPLE_FIELDS=$( echo $EXPERIMENT_BNS | xargs -n1 echo | xargs -n1 -I{} echo "{}_${FIELD}" | tr '\n' ' ' );
+    SAMPLE_FIELDS=$( echo $EXPERIMENTS | xargs -n1 echo | xargs -n1 -I{} echo "{}_${FIELD}" | tr '\n' ' ' );
 
     papolarity plot_distribution \
         "comparison/adjusted/all.tsv" \
