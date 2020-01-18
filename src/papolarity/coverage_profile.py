10f6910835de9dc77f3a826ccee55d3ae2a44db7
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-18 15:11:16 +0300

    remove unused function

--- src/papolarity/coverage_profile.py
+++ src/papolarity/coverage_profile.py
@@ -2,11 +2,6 @@ from . import coreutils_sort
 from .dto.transcript_coverage import TranscriptCoverage
 from .dto.coverage_interval import CoverageInterval
 
-def transcript_coverages_from_alignment(alignment, sort_transcripts=False, dtype=float):
-    bedgraph = make_coverage(alignment, sort_transcripts=sort_transcripts, dtype=dtype)
-    coverage_intervals = coverage_intervals_from_bedgraph(bedgraph, dtype=dtype)
-    yield from TranscriptCoverage.each_in_bedgraph(coverage_intervals, dtype=dtype)
-
 def make_coverage(alignment, sort_transcripts=False, stream=True, dtype=float):
     if sort_transcripts:
         # sort by chromosome (transcript_id in case of transcriptomic alignments)
