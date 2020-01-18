c07da96df5e8e27dca1c851bb9a4d139e798c5c7
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-18 16:02:25 +0300

    drop old process.sh; make protocol executable

--- process.sh
+++
@@ -1,5 +0,0 @@
-python3 extract_annotation.py gencode.vM22.basic.annotation.gtf.gz > gencode.vM22.cds_features.tsv
-python3 extract_profile.py gencode.vM22.cds_features.tsv source_data/alignments/METTL3_rna_Coots2017_m_r2.bam 
-
-# samtools view source_data/alignments/METTL3_rna_Coots2017_m_r2.bam > METTL3.tsv
-# perl gtf.pl METTL3.tsv > METTL3_coverage.tsv
