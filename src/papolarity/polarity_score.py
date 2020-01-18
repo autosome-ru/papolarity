0c671cdcbb31d5dedd57317f5b4522867e7b29a1
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-18 13:54:00 +0300

    rewrite polarity_score using numpy

--- src/papolarity/polarity_score.py
+++ src/papolarity/polarity_score.py
@@ -1,11 +1,7 @@
+import numpy as np
+
 def polarity_score(coverage):
-    total_coverage = 0
-    numerator = 0
-    for i in range(len(coverage)):
-        total_coverage = total_coverage + coverage[i]
-        numerator += coverage[i] * i
-    if total_coverage == 0:
-        return 0
-    polarity_score = numerator / total_coverage
-    normalized_score = (polarity_score * 2 / (len(coverage) - 1)) - 1
-    return normalized_score
+    coverage = np.array(coverage)
+    normalized_coverage = coverage / coverage.sum()
+    positions = np.linspace(-1, 1, len(coverage))
+    return np.dot(normalized_coverage, positions)
