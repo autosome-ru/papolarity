c041d1b0d0bc1408c4a7e411030d97efc04bdddc
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-01-22 17:08:45 +0300

    added raw data obtaining

--- protocol-paper-obtain-data.sh
+++ protocol-paper-obtain-data.sh
@@ -14,6 +14,19 @@ find . -type f -name '*.gz' -exec bash -c 'gzip -d {}' \;
 STAR --runMode genomeGenerate --genomeDir . --genomeFastaFiles ./GRCm38.primary_assembly.genome.fa --sjdbGTFfile ./gencode.vM23.basic.annotation.gtf --runThreadN 32
 cd ..
 
+# Obtain raw data
+
+SRR='SRR315616 SRR315617 SRR315618 SRR315619 SRR315601 SRR315602 SRR315612 SRR315613 SRR315614 SRR315615 SRR315604 SRR315605 SRR315606 SRR315607 SRR315608 SRR315609 SRR315610 SRR315611';
+prefetch $SRR;
+mkdir raw;
+for SRR_FILE in $SRR; do fastq-dump -O ./raw --gzip $SRR_FILE --split-files; done;
+cat ./raw/SRR315616_1.fastq.gz ./raw/SRR315617_1.fastq.gz ./raw/SRR315618_1.fastq.gz ./raw/SRR315619_1.fastq.gz > ./fastq/ES_noHR_noCH_ribo.fastq.gz;
+cat ./raw/SRR315601_1.fastq.gz ./raw/SRR315602_1.fastq.gz > ./fastq/ES_noHR_60sCH_ribo.fastq.gz;
+cat ./raw/SRR315612_1.fastq.gz ./raw/SRR315613_1.fastq.gz ./raw/SRR315614_1.fastq.gz ./raw/SRR315615_1.fastq.gz > ./fastq/ES_90sHR_60sCH_ribo.fastq.gz;
+cat ./raw/SRR315604_1.fastq.gz ./raw/SRR315605_1.fastq.gz ./raw/SRR315606_1.fastq.gz > ./fastq/ES_120sHR_60sCH_ribo.fastq.gz;
+cat ./raw/SRR315607_1.fastq.gz ./raw/SRR315608_1.fastq.gz ./raw/SRR315609_1.fastq.gz > ./fastq/ES_150sHR_60sCH_ribo.fastq.gz;
+cat ./raw/SRR315610_1.fastq.gz ./raw/SRR315611_1.fastq.gz > ./fastq/ES_180sHR_60sCH_ribo.fastq.gz;
+
 # Trimming
 
 mkdir ./trim
